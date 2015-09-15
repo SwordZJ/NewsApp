06cf0523358bdf75f994184fb78ddd2fdea0fe24
mrRightZJ <707739952@qq.com>
2015-09-15 15:00:06 +0800

    初始化项目

---
+++ NewsApp/NewsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsApp
+//
+//  Created by liuzhouji on 15/9/15.
+//  Copyright (c) 2015年 ZJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewsApp/NewsApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NewsApp
+//
+//  Created by liuzhouji on 15/9/15.
+//  Copyright (c) 2015年 ZJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
